463a3f3f13cf9375826dd0fdb5aaeac4a4b355ef
Mox <kimyj7756@gmail.com>
2019-12-26 23:16:04 +0900

    Revert "ㅇ"
    This reverts commit 1044ba084cb90fb31c4046567dcf8df7e822e341.

--- djangobackend/djangobackend/urls.py
+++ djangobackend/djangobackend/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
