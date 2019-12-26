284fc459e5fdb3a4d4f8ca04220aa6b14ccf0f36
Mox <kimyj7756@gmail.com>
2019-12-26 14:16:07 +0900

    해커톤 시작!!!(2019-12-26)

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          3팀 시작!!
+          3팀 시작!
         </p>
         <a
           className="App-link"
