6cedecf596b8b128c112c8a55cdf979c4bda68b3
Mox <kimyj7756@gmail.com>
2019-12-26 16:41:41 +0900

    강의 적용

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// handlingSubmit() 에서 사용
+import api from './api';
+import PostView from './Components/PostView';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          3팀 시작!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "",
+      content: "",
+      results: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getPosts()
+  }
+
+  async getPosts() {
+    const _results = await api.getAllPosts()
+    console.log(_results)
+    this.setState({results: _results.data})
+  }
+
+  handlingChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})    
+  }
+
+  handlingDelete = async (id) => {
+    await api.deletePost(id)
+    this.getPosts()
+  }
+
+  handlingSubmit = async (event) => {
+    event.preventDefault() // event의 기본적인 기능을 하지않게 함
+    let result = await api.createPost(
+      {
+        title: this.state.title,
+        content: this.state.content,
+      }
+    );
+    console.log("작성 완료!", result);
+    this.setState({title: '', content: ''})
+    this.getPosts()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container maxWidth="lg">
+          <div className="PostingSection">
+            <Paper className="PostingPaper">
+              <h2>대나무 숲 글 작성하기</h2>
+              <form className="PostingForm" onSubmit={this.handlingSubmit}>
+                <TextField
+                  id="outlined-name"
+                  label="글 제목"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.handlingChange}
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                {/* <br /> */}
+
+                {/* <textarea 
+                  name="content"
+                  value={this.state.content}
+                  onChange={this.handlingChange}
+                /> */}
+
+                <TextField
+                  id="outlined-name"
+                  label="본문"
+                  name="content"
+                  multiline
+                  rowsMax="4"
+                  value={this.state.content}
+                  onChange={this.handlingChange}
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                {/* <br /> */}
+                
+                <Button variant="outlined" color="primary" type="submit">제출하기</Button>
+              </form>
+            </Paper>
+          </div>
+
+          <div className="ViewSection">
+            {
+              this.state.results.map(
+                (post) =>
+                <Card className={'card'}>
+                  <CardContent>
+                    <Typography className={'card-title'} color="textSecondary" gutterBottom>
+                      {post.id}번째 대나무
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                      <PostView
+                      key={post.id}
+                      title={post.title}
+                      content={post.content}
+                      />
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button value={post.id} onClick={(event) => this.handlingDelete(post.id)} color="secondary" size="small">삭제하기</Button>
+                  </CardActions>
+               </Card>
+              )
+            }
+          </div>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
